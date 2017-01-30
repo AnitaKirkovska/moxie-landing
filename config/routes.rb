99842f6dc0b82ab6d6000e07d9175ded4828d8ef
Anita <anitakirkovska@gmail.com>
2017-01-30 16:26:42 +0100

    Disabled admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :models
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
 	root "subscriptions#new"
   resources :subscriptions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
