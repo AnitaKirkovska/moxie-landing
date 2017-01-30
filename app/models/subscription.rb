2c2d4b503cb1322930c801c3f05556b3527abf6a
Anita <anitakirkovska@gmail.com>
2017-01-30 20:02:34 +0100

    Unique email verifications

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ApplicationRecord
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 	validates :name, presence: true
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => "Не смее да е празно"
 end
