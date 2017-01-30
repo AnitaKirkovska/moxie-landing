78bca661ca38b501da72c54aa97dc0e809391f5e
Anita <anitakirkovska@gmail.com>
2017-01-30 01:07:12 +0100

    Admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 	root "subscriptions#new"
   resources :subscriptions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
